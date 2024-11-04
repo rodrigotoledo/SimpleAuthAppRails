cd45c3a20a8d8cc64624ccc3ce666221724163b6
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2024-11-04 13:32:06 -0300

    Add flash styling and implement inactivity controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root "home#index"
+  get "home", to: "home#index", as: :home
+  post "login", to: "sessions#create", as: :login
+  delete "logout", to: "sessions#destroy", as: :logout
+  root "sessions#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
